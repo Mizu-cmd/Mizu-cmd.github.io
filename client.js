f15fa313530fc253fb05bc5108072a3e6fcd07e2
Mizu-cmd <mithanielvillard@gmail.com>
2021-03-16 20:48:52 +0100

    fixed index

--- client.js
+++ client.js
@@ -40,5 +40,5 @@ window.addEventListener("DOMContentLoaded", function() {
 document.body.addEventListener('click', clicked, true); 
 
 function clicked(){
-  window.location = "public/index.html"
+  window.location = "public/public.html"
 }
